a870cac350e2edc126bfd0ef91bcc49bc92d2ed9
buildguy <buildguy@pentaho.com>
2012-05-17 15:34:05 -0400

    Lee's recipe

---
+++ recipes/beer-brats.js
@@ -0,0 +1,15 @@
+pentahoRecipeBook.recipes.push({
+  author: "Lee Panda Cheng", 
+  title: "Beer Bratwurst", 
+  description: "Fat and juicy!",
+  ingredients: [
+    {quantity: '6', name: 'pork bratwurst'},
+    {quantity: '1', name: 'sweet yellow onions'},
+    {quantity: '1 can', name: 'Guiness'}
+  ],
+  steps: [
+    "Turn on the grill - make it hot",
+    "Slice onion. Put onion, brats, and beer into a foil pan, cover the pan with foil",
+    "Poke vent hole in foil, place foil pan on the grill.  Let simmer for 20 minutes."
+  ]
+});
